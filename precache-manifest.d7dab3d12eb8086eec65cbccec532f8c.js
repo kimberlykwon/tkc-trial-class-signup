2869626b57e9a4d8750ece67641573be1077362b
kimberlykwon <kimberlykwon2019@u.northwestern.edu>
2019-09-19 03:12:29 -0500

    Updates

---
+++ precache-manifest.d7dab3d12eb8086eec65cbccec532f8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5ff923bd5fde53373a34d3c98d7fe08",
+    "url": "/j/index.html"
+  },
+  {
+    "revision": "0b7347566314a8483292",
+    "url": "/j/static/css/main.4cc069e5.chunk.css"
+  },
+  {
+    "revision": "d41e1bd91cd9ea604197",
+    "url": "/j/static/js/2.6c1d48de.chunk.js"
+  },
+  {
+    "revision": "0b7347566314a8483292",
+    "url": "/j/static/js/main.f112616f.chunk.js"
+  },
+  {
+    "revision": "86cceefbb30e076ad94b",
+    "url": "/j/static/js/runtime~main.b16d7bee.js"
+  },
+  {
+    "revision": "54413459a5adf3f82686db50595cba5a",
+    "url": "/j/static/media/BebasNeue-Regular.54413459.ttf"
+  }
+]);

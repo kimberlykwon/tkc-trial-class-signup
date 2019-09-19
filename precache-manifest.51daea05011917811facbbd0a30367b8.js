9cc426fd8d6397b37fc4132a65e59489f1ea8b56
kimberlykwon <kimberlykwon2019@u.northwestern.edu>
2019-09-19 03:35:44 -0500

    Updates

---
+++ precache-manifest.51daea05011917811facbbd0a30367b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa16da8dcf60e25180776e166496fe08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f1ca31efe77a1041997",
+    "url": "/static/css/main.e520444d.chunk.css"
+  },
+  {
+    "revision": "d41e1bd91cd9ea604197",
+    "url": "/static/js/2.6c1d48de.chunk.js"
+  },
+  {
+    "revision": "1f1ca31efe77a1041997",
+    "url": "/static/js/main.61c96ae3.chunk.js"
+  },
+  {
+    "revision": "8cb9ce6d5e7a44035f6e",
+    "url": "/static/js/runtime~main.6da060c2.js"
+  },
+  {
+    "revision": "54413459a5adf3f82686db50595cba5a",
+    "url": "/static/media/BebasNeue-Regular.54413459.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/j/precache-manifest.d7dab3d12eb8086eec65cbccec532f8c.js"
+  "/precache-manifest.51daea05011917811facbbd0a30367b8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/j/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

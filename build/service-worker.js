34e59af51c37eb01c2243897887f3501987e4f87
kimberlykwon <kimberlykwon2019@u.northwestern.edu>
2019-09-19 03:17:14 -0500

    router

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.76b608d028a0141c3ee236670796f7f5.js"
+  "/j/precache-manifest.d7dab3d12eb8086eec65cbccec532f8c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/j/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
--- src/index.js
+++ src/index.js
@@ -9,13 +9,13 @@ import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 // ReactDOM.render(<App />, document.getElementById('root'));
 
 const routing = (
-  <Router>
+  <BrowserRouter>
     <div>
       <Route path="/" exact component={App} />
       <Route path="/j" component={App} />
       <Route path="/k" component={KidsApp} />
     </div>
-  </Router>
+  </BrowserRouter>
 )
 ReactDOM.render(routing, document.getElementById('root'))
 // If you want your app to work offline and load faster, you can change

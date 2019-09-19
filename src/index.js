704ebc7f6070009ada9e6db29ffdeecfa17e04d6
kimberlykwon <kimberlykwon2019@u.northwestern.edu>
2019-09-19 03:57:54 -0500

    hi

--- src/index.js
+++ src/index.js
@@ -6,18 +6,18 @@ import KidsApp from './KidsApp';
 import * as serviceWorker from './serviceWorker';
 import { Route, Link, HashRouter, BrowserRouter as Router } from 'react-router-dom'
 
-// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
-const routing = (
-  <HashRouter basename = {process.env.PUBLIC_URL} history = {hashHistory}>
-    <div>
-      <Route path="/" exact component={App} />
-      <Route path="/j" component={App} />
-      <Route path="/k" component={KidsApp} />
-    </div>
-  </HashRouter>
-)
-ReactDOM.render(routing, document.getElementById('root'))
+// const routing = (
+//   <HashRouter basename = {process.env.PUBLIC_URL} >
+//     <div>
+//       <Route path="/" exact component={App} />
+//       <Route path="/j" component={App} />
+//       <Route path="/k" component={KidsApp} />
+//     </div>
+//   </HashRouter>
+// )
+// ReactDOM.render(routing, document.getElementById('root'))
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
